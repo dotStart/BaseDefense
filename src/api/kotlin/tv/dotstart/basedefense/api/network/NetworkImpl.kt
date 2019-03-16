260f7bc1a3690d249aaadbc415d287a4ac91164d
Johannes Donath <johannesd@torchmind.com>
2019-03-16 09:24:59 +0100

    Created an unfinished network implementation.

---
+++ src/api/kotlin/tv/dotstart/basedefense/api/network/NetworkImpl.kt
@@ -0,0 +1,39 @@
+package tv.dotstart.basedefense.api.network
+
+import tv.dotstart.basedefense.api.device.SecurityComponent
+import tv.dotstart.basedefense.api.device.SecurityDevice
+import tv.dotstart.basedefense.api.network.event.Event
+import tv.dotstart.basedefense.api.util.PlayerReference
+
+/**
+ * Provides a standard network implementation.
+ */
+open class NetworkImpl(override val owner: PlayerReference) : Network {
+
+  override var controller: SecurityDevice? = null
+
+  override var active = false
+  override val components: MutableSet<SecurityComponent> = mutableSetOf()
+
+  override fun plusAssign(component: SecurityComponent) {
+    this.components += component
+    // TODO: Event
+  }
+
+  override fun plusAssign(network: Network) {
+    TODO("not implemented")
+  }
+
+  override fun minusAssign(component: SecurityComponent) {
+    this.components -= component
+    // TODO: Event
+  }
+
+  override fun postEvent(event: Event) {
+    TODO("not implemented")
+  }
+
+  override fun canConnect(device: SecurityComponent): Boolean {
+    return this.owner == device.owner
+  }
+}
